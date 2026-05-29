520f2891b110d0a8d877e380446821cf988648bd
Gustavo Totti <gustavo.totti14@gmail.com>
2026-05-29 00:09:40 -0300

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose')
 
 async function main() {
-
-    await mongoose.connect(
-        `mongodb+srv://${process.env.GTmgbr}:${process.env.1234}@cluster0.s2be24w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    )
-
-    console.log('Conectou ao MongoDB Atlas!')
+    await mongoose.connect('mongodb://localhost:27017/getapet')
+    console.log('Conectou ao Mongoose!')
 }
 
 main().catch((err) => console.log(err))
